95b9c244b4c943f49267702d8b393cf6b9234f0f
aAnfitrion <tendondevastador@hotmail.com>
2022-08-23 17:31:29 -0500

    Ampliando el espacio de las columnas

--- components/index/MainPresentation.js
+++ components/index/MainPresentation.js
@@ -1,13 +1,13 @@
 export default function MainPresentation() {
   return(
     <div className='py-8 bg-yaru-600 border-b border-yaru-500'>
-      <div className='grid grid-cols-6 my-10'>
-        <div className='flex justify-center col-start-2 col-span-2'>
+      <div className='grid grid-cols-8 my-10'>
+        <div className='flex justify-center col-start-2 col-span-3'>
           <div className='p-3 bg-yaru-700 rounded-xl border border-yaru-200'>
             <img src='/img/golem.png' />
           </div>
         </div>
-        <div className='flex items-center justify-center col-span-2'>
+        <div className='flex items-center justify-center col-span-3'>
           <div className='text-center'>
             <h1 className='text-6xl mb-3'>Anfitrion</h1>
             <p>En esta página podrás encontrar información sobre mi. Tal como: mis habilidades, mis formas de contacto, mis proyectos y otras cosas más. Hice esta página porque vi un video de portafolios impresionantes, y quiero hacer mi propio portafolio.</p>
