006a490efd8c59ca230df6d95659ec9eb221601e
aAnfitrion <tendondevastador@hotmail.com>
2023-06-14 09:38:41 -0500

    Creamos un mini componente para ayudarnos a la creación de +page

---
+++ src/routes/proyectos/Li.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let href: string, titulo: string, desc: string;
+</script>
+
+<li>
+	<a {href}>
+		<span class="h2">
+			<slot />
+		</span>
+		<span class="flex-auto hover:pl-4 duration-200">
+			<dt class="h2 font-semibold">{titulo}</dt>
+			<dd>{desc}</dd>
+		</span>
+	</a>
+</li>
